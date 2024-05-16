8fac025d3089a3eebca8f09f112f6692f246bf10
Igor Babko <i.i.babko@gmail.com>
2024-05-16 22:49:43 +0300

    .

--- init.lua
+++ init.lua
@@ -8,10 +8,10 @@ vim.cmd('nmap <leader>c :e ~/.config/nvim/init.lua<cr>')
 vim.cmd('nmap <leader>s :w<cr>')
 
 -- motion keys (left, down, up, right)
-vim.keymap.set({ 'n', 'v' }, 'j', 'h')
-vim.keymap.set({ 'n', 'v' }, 'k', 'j')
-vim.keymap.set({ 'n', 'v' }, 'l', 'k')
-vim.keymap.set({ 'n', 'v' }, ';', 'l')
+vim.keymap.set({'n', 'v'}, 'j', 'h')
+vim.keymap.set({'n', 'v'}, 'k', 'j')
+vim.keymap.set({'n', 'v'}, 'l', 'k')
+vim.keymap.set({'n', 'v'}, ';', 'l')
 
 -- repeat previous f, t, F or T movement
 vim.keymap.set('n', '\'', ';')
@@ -41,22 +41,31 @@ vim.opt.smartcase = true
 -- install lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', -- latest stable release
+         lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  {
+require("lazy").setup({{
     'vscode-neovim/vscode-multi-cursor.nvim',
     event = 'VeryLazy',
     cond = not not vim.g.vscode,
-    opts = {},
-  }
+    opts = {}
+}})
+
+vim.keymap.set('n', '<c-d>', 'mciw*:nohl<cr>', {
+    remap = true
 })
+
+vim.keymap.set({"n", "x", "i"}, "<C-d>", function()
+    require("vscode-multi-cursor").addSelectionToNextFindMatch()
+end)
+
+vim.keymap.set({"n", "x", "i"}, "<CS-d>", function()
+    require("vscode-multi-cursor").addSelectionToPreviousFindMatch()
+end)
+
+vim.keymap.set({"n", "x", "i"}, "<CS-l>", function()
+    require("vscode-multi-cursor").selectHighlights()
+end)

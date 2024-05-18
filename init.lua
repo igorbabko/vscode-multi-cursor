f3ee52d97ef793dc0dea8dc111e82f7803e2f79f
Igor Babko <i.i.babko@gmail.com>
2024-05-18 18:55:28 +0300

    04. Wrapped VS Code commands

--- init.lua
+++ init.lua
@@ -54,3 +54,17 @@ require('lazy').setup({{
     cond = not not vim.g.vscode,
     opts = {}
 }, 'folke/flash.nvim'})
+
+local cursors = require('vscode-multi-cursor')
+
+vim.keymap.set({'n', 'x', 'i'}, '<c-d>', function()
+    cursors.addSelectionToNextFindMatch()
+end)
+
+vim.keymap.set({'n', 'x', 'i'}, '<cs-d>', function()
+    cursors.addSelectionToPreviousFindMatch()
+end)
+
+vim.keymap.set({'n', 'x', 'i'}, '<cs-l>', function()
+    cursors.selectHighlights()
+end)
